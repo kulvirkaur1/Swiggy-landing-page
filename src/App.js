f969342b9346bfa479474886f3c8d63d2be5035f
kulvir kaur <89350498+kulvirkaur1@users.noreply.github.com>
2022-03-03 19:24:39 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import Nav from "./components/Hero/Nav";
+import Order from "./components/OrderTrack/Order";
+import Mobile from "./components/Restorent/Mobile";
+import Last from "./components/Footer/Last";
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+     
+     <Nav />
+     <Order />
+     <Mobile />
+     <Last />
+
+    
+    </div>
+  );
+}
+
+export default App;
